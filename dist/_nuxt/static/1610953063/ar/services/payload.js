1017a3d201271aba88accd084907f0a5766a99bc
Fawzy Mohamed <fmohamed@alkholi.com>
2023-08-13 10:45:19 +0300

    generate the website

--- dist/_nuxt/static/1610953063/ar/services/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/ar/services", {data:[{}],fetch:[],mutations:void 0});
---
+++ dist/_nuxt/static/1691912682/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Far\u002Fservices","\u002Fservices","\u002Far","\u002Far\u002Fabout"]})
